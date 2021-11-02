011f03be51e542576ef0f1cbb29c5d5205a6e080
Samuel Melton <smelton8720@gmail.com>
2021-11-01 22:20:02 -0500

    added function comments

--- problem14_LongestCollatzSequence.py
+++ problem14_LongestCollatzSequence.py
@@ -15,47 +15,71 @@ def get_next_collatz(number):
         return 3 * number + 1
 
 def gen_collatz_number(starting_number):
+    """Generate the next number in the collatz sequence given a starting number."""
     next_num = get_next_collatz(starting_number)
     yield next_num
     while True:
         next_num = get_next_collatz(next_num)
         yield next_num
 
-def update_collatz_dict(collatz_dict, collatz_sequence):
-    if collatz_sequence[0] not in collatz_dict.keys():
-        collatz_dict.update({collatz_sequence[0]: len(collatz_sequence)})
+def add_collatz_que_to_collatz_dict(collatz_queue, offset_value, collatz_dict):
+    """Add the collatz_queue values to the collatz_dict."""
+    counter = 1
+    collatz_queue.reverse()
+    for num in collatz_queue:
+        sequence_count = counter + offset_value
+        collatz_dict.update({num : sequence_count})
+        counter += 1
     return collatz_dict
 
-def get_collatz_sequence(starting_num, collatz_dict):
-    collatz_sequence = [starting_num]
+def updated_collatz_dict_with_sequence_starting_at(starting_num, collatz_dict):
+    """
+    Generate the collatz sequence for a given starting number.
+    Add any unknown values to the collatz_dict and return it. 
+    """
+    assert 1 in collatz_dict
+    collatz_queue = []
     gen = gen_collatz_number(starting_num)
     current_num = starting_num
-    while current_num != 1 and current_num not in collatz_dict.keys():
+    offset = 0
+    while True:
+        if current_num in collatz_dict:
+            offset = collatz_dict[current_num]
+            break
+        collatz_queue.append(current_num)
         current_num = next(gen)
-        collatz_sequence.append(current_num)
-    updated_dict = update_collatz_dict(collatz_dict, collatz_sequence)
+    updated_dict = add_collatz_que_to_collatz_dict(collatz_queue, offset, collatz_dict)
     return updated_dict
 
-def get_ans(collatz_dict):
+def parse_dict_for_answer(collatz_dict):
+    """Return the key with the highest integer value for the given dictionary."""
     key_list = list(collatz_dict.keys())
     value_list = list(collatz_dict.values())
-    max_value = max(value_list)
-    max_value_index = value_list.index(max_value)
-    val = key_list[max_value_index]
-    return val
+    max_sequence_count = max(value_list)
+    max_value_index = value_list.index(max_sequence_count)
+    ans = key_list[max_value_index]
+    return ans
+
+def print_percent_complete(progress):
+    """Print the calculation percentage status given the solution progress as a fraction."""
+    decimal = progress * 100
+    status = round(decimal, 2)
+    print(f"{status}%")
 
 def find_longest_collatz_sequence_under(max_number):
-    # starting num, sequence count
+    """Return the starting number below the given max_number that has the most terms in its collatz sequence."""
+    # starting num : sequence count
+    # must pre-populate for updated_collatz_dict_with_sequence_starting_at to work right.
     collatz_dict = {1: 1}
     index = 1
     while index < max_number:
-        collatz_dict = get_collatz_sequence(index, collatz_dict)
+        if index not in collatz_dict:
+            collatz_dict = updated_collatz_dict_with_sequence_starting_at(index, collatz_dict)
         index += 1
-        print(index)
-    return get_ans(collatz_dict)
+        print_percent_complete(index/max_number)
+    return parse_dict_for_answer(collatz_dict)
 
 
 if __name__ == "__main__":
-    print(f"Answer to Project Euler problem #14 is: {find_longest_collatz_sequence_under(1000000)}")
-
-# getting 626331 but it's not right.
+    max_num = 1000000
+    print(f"Answer to Project Euler problem #14 is: {find_longest_collatz_sequence_under(max_num)}")
