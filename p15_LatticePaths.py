e255f4c13765dfedadb35a9f59c9926ab941ba51
Samuel <smelton@sw-construction.com>
2021-11-03 15:56:39 -0500

    continued work, got nowhere

--- p15_LatticePaths.py
+++ p15_LatticePaths.py
@@ -1,54 +1,27 @@
 
 
+def get_ans(positions):
+    entangled_relationships_count = positions // 2
+    available_entanglements = positions - 1
+    valid_configurations_count = 0
+
+    for position in range(0, positions - 1):
+        valid_configurations_count += entangled_relationships_count * available_entanglements
+        available_entanglements -= 1
+
+    return valid_configurations_count
+    
+def recur_ans(free_spots):
+    if free_spots == 2:
+        return 1
+    else:
+        counter = (free_spots - 1) + recur_ans(free_spots - 2)
+        return counter
+
+
+
+
 # Whatever is the most far right, and farthest up (as long as it's not the last right segment) delete it and add 1
-# This is kind of a hard problem to model... 
-
-
-def get_unknown_direction_count(sequence_size, known_direction_count):
-    return sequence_size - known_direction_count
-
-def get_initial_sequence(sequence_size):
-    current_sequence = []
-    r_count = sequence_size // 2
-    d_count = get_unknown_direction_count(sequence_size, r_count)
-    for r in range(0, r_count):
-        current_sequence.append('r')
-    for d in range(0, d_count):
-        current_sequence.append('d')
-    return current_sequence
-
-def is_valid_sequence(sequence):
-    r_count = sequence.count('r')
-    d_count = sequence.count('d')
-    if r_count == d_count:
-        return True
-    else:
-        return False
-
-# This won't get all variations. need to improve logic and likely make it keep varying until it finds new sequence (memoization required)
-def get_sequence_variation(sequence, initial_sequence):
-    r_to_change = sequence.index('r')
-    d_to_change = sequence.index('d')
-    sequence[r_to_change] = 'd'
-    sequence[d_to_change] = 'r'
-    if is_valid_sequence(sequence) and sequence != initial_sequence:
-        return sequence
-    else:
-        raise NotImplementedError
-
-def get_valid_sequence_count(sequence_size):
-    all_valid_sequences = []
-    initial_sequence = get_initial_sequence(sequence_size)
-    temp_sequence = initial_sequence.copy()
-    all_valid_sequences.append(temp_sequence)
-    while True:
-        try:
-            temp_sequence = get_sequence_variation(temp_sequence, initial_sequence)
-            all_valid_sequences.append(temp_sequence)
-        except:
-            break
-    return len(all_valid_sequences)
-
 if __name__ == "__main__":
     sequence_size = 4
-    print(f"Answer to Project Euler problem #15 is: {get_valid_sequence_count(sequence_size)}")
+    print(f"Answer to Project Euler problem #15 is: {recur_ans(sequence_size)}")
